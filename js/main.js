b39449a6a669df8b823230454c2a1c3bece36ada
tmadhani <tmadhani@umich.edu>
2018-04-16 14:05:25 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("ShowImage").onclick = function() {
+    document.getElementById("one").style.visibility = "visible";
+}
+
+document.getElementById("HideImage").onclick = function() {
+    document.getElementById("one").style.visibility = "hidden";
+}
